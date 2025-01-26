80679a6cdc29fd3bf34e1a22ab5b47f002008240
Logan Mills <ppa9ed@virginia.edu>
2025-01-26 13:38:07 -0500

    Submission

---
+++ Objects/main.d
@@ -0,0 +1,5 @@
+.\objects\main.o: Main.c
+.\objects\main.o: C:\Users\apple\AppData\Local\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdio.h
+.\objects\main.o: C:\Users\apple\AppData\Local\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
+.\objects\main.o: PLL.h
+.\objects\main.o: UART.h
---
+++ Objects/uart.d
@@ -0,0 +1,5 @@
+.\objects\uart.o: UART.c
+.\objects\uart.o: C:\Users\apple\AppData\Local\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdio.h
+.\objects\uart.o: C:\Users\apple\AppData\Local\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
+.\objects\uart.o: UART.h
+.\objects\uart.o: tm4c123gh6pm.h
